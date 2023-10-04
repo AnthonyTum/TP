930dfde9d2265e7a6f8b1c2f29afe9fdcc132ce5
AnthonyTum <146720457+AnthonyTum@users.noreply.github.com>
2023-10-04 22:33:31 +0200

    Update carousel.js

--- js/carousel.js
+++ js/carousel.js
@@ -1,20 +1,20 @@
-const buttonsWrapper = document.querySelector(".map");
-const slides = document.querySelector(".inner");
+const carouselContainer = document.querySelector(".carousel-container");
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
 
-buttonsWrapper.addEventListener("click", e => {
-  if (e.target.nodeName === "BUTTON") {
-    Array.from(buttonsWrapper.children).forEach(item =>
-      item.classList.remove("active")
-    );
-    if (e.target.classList.contains("first")) {
-      slides.style.transform = "translateX(-0%)";
-      e.target.classList.add("active");
-    } else if (e.target.classList.contains("second")) {
-      slides.style.transform = "translateX(-33.33333333333333%)";
-      e.target.classList.add("active");
-    } else if (e.target.classList.contains('third')){
-      slides.style.transform = 'translatex(-66.6666666667%)';
-      e.target.classList.add('active');
-    }
-  }
+let currentIndex = 0;
+
+nextBtn.addEventListener("click", () => {
+    currentIndex = (currentIndex + 1) % carouselContainer.children.length;
+    updateCarousel();
 });
+
+prevBtn.addEventListener("click", () => {
+    currentIndex = (currentIndex - 1 + carouselContainer.children.length) % carouselContainer.children.length;
+    updateCarousel();
+});
+
+function updateCarousel() {
+    const offset = -currentIndex * 1280;
+    carouselContainer.style.transform = `translateX(${offset}px)`;
+}
