07eaa23a4c84fdadf08f1046dba4a34ea7332762
Ronny <ronny.louis-marie@epitech.eu>
2023-10-05 13:10:20 +0200

    add form js

--- js/form.js
+++ js/form.js
@@ -1,17 +1,20 @@
+
 const box = document.getElementById("form1");
 const label = document.getElementById("lblusername");
+
 
 function check(input) {
     const notGood = document.getElementById('notGood');
   
-    if (input.value.length > 5 && !notGood) {
+    if (input.value.length < 5 && !notGood) {
       // L'input est invalide
       const red = document.createElement('span');
       red.id = "notGood"
       red.classList.add('red');
-      red.innerText = " - Trop long"
+      red.innerText = " Pseudo Trop court"
+      red.style="color:red"
       label.appendChild(red);
-  } else if (input.value.length <= 5 && notGood) {
+  } else if (input.value.length >= 5 && notGood) {
       // L'input est valide
       notGood.remove()
   }
