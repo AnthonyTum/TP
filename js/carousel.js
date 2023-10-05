12bdde2f088106075118f810e84819c30aba3384
Ronny <ronny.louis-marie@epitech.eu>
2023-10-05 13:22:46 +0200

    carousel test

---
+++ js/carousel.js
@@ -0,0 +1,30 @@
+onst carouselContainer = document.querySelector(".carousel-container");
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+const children = document.querySelector(".carousel-inner");
+
+let currentIndex = 0;
+
+nextBtn.addEventListener("click", () => {
+    currentIndex++;
+    console.log(children.children.length)
+    if (currentIndex >= children.children.length) {
+        currentIndex = 0;
+        console.log("reset")
+    }
+    updateCarousel();
+});
+
+prevBtn.addEventListener("click", () => {
+    currentIndex--;
+    if (currentIndex < 0) {
+        currentIndex = children.children.length - 1;
+    }
+    updateCarousel();
+});
+
+function updateCarousel() {
+    const offset = -currentIndex * 450;
+    carouselContainer.style.transform = `translateX(${offset}px)`;
+    console.log(carouselContainer.style.transform, offset)
+}
