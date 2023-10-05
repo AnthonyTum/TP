ac34e5610db0a4c606203fad4ddeabd1300cc08a
Ronny <ronny.louis-marie@epitech.eu>
2023-10-05 12:45:13 +0200

    js

---
+++ js/form.js
@@ -0,0 +1,18 @@
+const box = document.getElementById("form1");
+const label = document.getElementById("lblusername");
+
+function check(input) {
+    const notGood = document.getElementById('notGood');
+  
+    if (input.value.length > 5 && !notGood) {
+      // L'input est invalide
+      const red = document.createElement('span');
+      red.id = "notGood"
+      red.classList.add('red');
+      red.innerText = " - Trop long"
+      label.appendChild(red);
+  } else if (input.value.length <= 5 && notGood) {
+      // L'input est valide
+      notGood.remove()
+  }
+}
